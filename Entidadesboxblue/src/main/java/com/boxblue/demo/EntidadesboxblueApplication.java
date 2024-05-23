dc659f844d21fc8913a6daf2e9edf47150a022fb
Lucas Prestes <132359745+Mysteeeeel@users.noreply.github.com>
2024-05-23 15:42:56 -0300

    Add files via upload

---
+++ Entidadesboxblue/src/main/java/com/boxblue/demo/EntidadesboxblueApplication.java
@@ -0,0 +1,13 @@
+package com.boxblue.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadesboxblueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadesboxblueApplication.class, args);
+	}
+
+}
